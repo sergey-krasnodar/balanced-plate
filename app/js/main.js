bdc73e4d8bee5b02b82756ede54b633ed764f215
sergey-krasnodar <serega.ru71@mail.ru>
2025-08-18 09:34:23 +0300

    add modal and fixed header

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+
+const btnOpen = document.querySelector('.header__btn-phone'); 
+const modalPhone = document.getElementById('modal'); 
+
+btnOpen.addEventListener('click', () =>{
+
+  modalPhone.showModal()
+})
+
+modalPhone.addEventListener('click', (e) => {
+  if (e.target === modalPhone) modalPhone.close()
+})
